e88f6ce4a288040c0bf2fa4642c9a906ad983758
Ankit Jain <ankitjain@zadv.com>
2016-08-02 17:14:19 -0400

    Removed CSS bundle

--- Source/ZADV.ZLeaderboard/ZADV.ZLeaderboard.Web/App_Start/BundleConfig.cs
+++ Source/ZADV.ZLeaderboard/ZADV.ZLeaderboard.Web/App_Start/BundleConfig.cs
@@ -26,12 +26,6 @@ namespace ZADV.ZLeaderboard.Web
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/font-awesome.min.css",
-                      "~/Content/site.css",
-                      "~/Content/datetimepicker.css"));
         }
     }
 }
